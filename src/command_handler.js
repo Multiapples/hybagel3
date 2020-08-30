d97b074f39a0aac2a5a8cf45c3824e0cb18afcc7
Multiapples <47487689+Multiapples@users.noreply.github.com>
2020-08-30 15:28:30 -0400

    Chore/ Partitioned and refactored code
    Refactored message_handler.js and partitioned part of it's code into command_handler.js

---
+++ src/command_handler.js
@@ -0,0 +1,28 @@
+const { prefix } = require('./config.json');
+
+module.exports = {
+	handleCommandCall(message) {
+		const commandName = isolateCommandName(message);
+		if (isCallingInvalidCommand(message, commandName)) return;
+		const commandArgString = isolateCommandArgString(message, commandName);
+		callCommand(message, commandName, commandArgString);
+	},
+};
+
+function isolateCommandName(message) {
+	return message.content.split(' ')[0].slice(prefix.length);
+}
+function isolateCommandArgString(message, commandName) {
+	return message.content.slice(prefix.length + commandName.length).trim();
+}
+function isCallingInvalidCommand(message, commandName) {
+	return !message.client.commandModules.has(commandName);
+}
+function callCommand(message, commandName, commandArgString) {
+	const command = message.client.commandModules.get(commandName);
+	if (command.doSplitArgs) {
+		command.execute(message, commandArgString);
+	} else {
+		command.execute(message, commandArgString.split(/ +/));
+	}
+}
--- src/message_handler.js
+++ src/message_handler.js
@@ -1,34 +1,17 @@
+const CommandHandler = require('./command_handler');
 const { prefix } = require('./config.json');
 
 module.exports = {
 	handleMessage(message) {
-		if (message.author.bot) return;
-		if (!message.content.startsWith(prefix)) return
-		const nameAndArgString = stripNameAndArgumentString(message);
-		if (message.client.commandModules.has(nameAndArgString.name))
-			return executeCommand(message, nameAndArgString.name, nameAndArgString.argString);
+		if (doesNotStartWithPrefix(message)) return;
+		if (isAuthorBot(message)) return;
+		CommandHandler.handleCommandCall(message);
 	},
 };
 
-function stripNameAndArgumentString(message) {
-	const name = message.content.split(' ')[0].slice(prefix.length);
-	const argString = message.content.slice(name.length).trim();
-	return {
-		name: name,
-		argString: argString,
-	};
+function doesNotStartWithPrefix(message) {
+	return !message.content.startsWith(prefix);
 }
-function executeCommand(message, name, argString) {
-	const command = getCommand(message, name)
-	if (command.doSplitArgs) {
-		command.execute(message, splitArgumentString(argString));
-	} else {
-		command.execute(message, argString);
-	}
-}
-function getCommand(message, commandName) {
-	return message.client.commandModules.get(commandName);
-}
-function splitArgumentString(argString) {
-	return argString.split(/ +/);
+function isAuthorBot(message) {
+	return message.author.bot;
 }
