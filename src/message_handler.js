a0e0ab44c6ebdfa212b26b539cc75459f3b2f2d3
Multiapples <47487689+Multiapples@users.noreply.github.com>
2020-08-30 13:51:14 -0400

    Chore/ Slight code refactor

--- src/message_handler.js
+++ src/message_handler.js
@@ -6,21 +6,10 @@ module.exports = {
 		if (!message.content.startsWith(prefix)) return
 		const nameAndArgString = stripNameAndArgumentString(message);
 		if (message.client.commandModules.has(nameAndArgString.name))
-			return executeCommand(message, nameAndArgString);
+			return executeCommand(message, nameAndArgString.name, nameAndArgString.argString);
 	},
 };
 
-function executeCommand(message, nameAndArgString) {
-	const command = getCommand(message, nameAndArgString.name)
-	if (command.doSplitArgs) {
-		command.execute(message, splitArgumentString(nameAndArgString.argString));
-	} else {
-		command.execute(message, nameAndArgString.argString);
-	}
-}
-function getCommand(message, commandName) {
-	return message.client.commandModules.get(commandName);
-}
 function stripNameAndArgumentString(message) {
 	const name = message.content.split(' ')[0].slice(prefix.length);
 	const argString = message.content.slice(name.length).trim();
@@ -28,6 +17,17 @@ function stripNameAndArgumentString(message) {
 		name: name,
 		argString: argString,
 	};
+}
+function executeCommand(message, name, argString) {
+	const command = getCommand(message, name)
+	if (command.doSplitArgs) {
+		command.execute(message, splitArgumentString(argString));
+	} else {
+		command.execute(message, argString);
+	}
+}
+function getCommand(message, commandName) {
+	return message.client.commandModules.get(commandName);
 }
 function splitArgumentString(argString) {
 	return argString.split(/ +/);
