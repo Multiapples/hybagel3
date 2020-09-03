68301b537c48cd40eaeb28201b09194ec9c92099
Multiapples <47487689+Multiapples@users.noreply.github.com>
2020-09-03 01:07:38 -0400

    Chore/ Remove unnecessary args

--- src/commands/version.js
+++ src/commands/version.js
@@ -1,10 +1,10 @@
-const stringArgCommand = require('./../command_base_classes/command/string_arg_command');
+const stringArgCommand = require('./../command_base_classes/command/string_arg_command.js');
 const currentVersion = require('./../config.json').version;
 
 module.exports = class version extends stringArgCommand {
 	static name = 'version';
 	static description = 'Gives you the current version of the bot';
-	static execute(message, args) {
+	static execute(message) {
 		return message.channel.send(`Current version: v${currentVersion}.`);
 	}
 }
